3aab1e8a21bdaa8873e5156d30b340ad5788569d
i-bulat <85643596+i-bulat@users.noreply.github.com>
2022-08-23 12:16:06 +0000

    alert box added

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,22 +1,27 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceHolderDirective } from '../shared/placeholder/placeholder.directive';
 import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
+  @ViewChild(PlaceHolderDirective, {static: false}) alertHost: PlaceHolderDirective;
+
+  private closeSub: Subscription;
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
     ) {}
 
   onSwitchMode() {
@@ -47,10 +52,34 @@ export class AuthComponent {
       error: (e) => {
         console.log(e)
         this.error = e;
+        this.showErrorAlert(e);
         this.isLoading = false},
 
       });
     form.reset();
   }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  ngOnDestroy(): void {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
+  }
+
+  private showErrorAlert(message: string) {
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+
+   const componentRef = hostViewContainerRef.createComponent<AlertComponent>(AlertComponent)
+  componentRef.instance.message = message;
+  this.closeSub = componentRef.instance.close.subscribe(() => {
+    this.closeSub.unsubscribe()
+    hostViewContainerRef.clear();
+  })}
+
+
 
 }
